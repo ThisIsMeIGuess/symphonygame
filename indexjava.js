57e761bb08af244378900308fece86a24f50a29e
ThisIsMeIGuess <jdamon@college.harvard.edu>
2025-01-28 22:11:52 -0500

    Add files via upload

---
+++ indexjava.js
@@ -0,0 +1,7 @@
+// Play music when the page is loaded.
+
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = new Audio('https://res.cloudinary.com/dm8attfdo/video/upload/v1738119995/mahler_3_index_dryglo.mp3');
+    // Play the audio
+    audio.play();
+})
