3bdf5cb984ed210dd67f11185375d466a7b0539b
ThisIsMeIGuess <jdamon@college.harvard.edu>
2025-02-04 23:11:10 -0500

    Update mainpagejava.js

--- mainpagejava.js
+++ mainpagejava.js
@@ -1,13 +1,13 @@
 // When the user's mouse goes over the picture of Sibelius, it turns into Mahler.
 
 document.addEventListener('DOMContentLoaded', function() {
-    sibelius.addEventListener('mouseover', function() {
+    sibelius.addEventListener('click', function() {
         sibelius.style.opacity = 0.0;
     })
 })
 
 document.addEventListener('DOMContentLoaded', function() {
-    sibelius.addEventListener('mouseout', function() {
+    sibelius.addEventListener('click', function() {
         sibelius.style.opacity = 1.0;
     })
 })
