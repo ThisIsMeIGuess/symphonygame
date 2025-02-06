b7a1f80f995a8f3876af211f983b85f12472465d
ThisIsMeIGuess <jdamon@college.harvard.edu>
2025-02-05 20:05:03 -0500

    Update mainpagejava.js

--- mainpagejava.js
+++ mainpagejava.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     sibelius.addEventListener('mouseover', function() {
         sibelius.style.opacity = 0.0;
     })
+    sibelius.addEventListener('touchstart', function() {
+        sibelius.style.opacity = 0.0;
+    })
+
 })
 
 document.addEventListener('DOMContentLoaded', function() {
     sibelius.addEventListener('mouseout', function() {
+        sibelius.style.opacity = 1.0;
+    })
+    sibelius.addEventListener('touchend', function() {
         sibelius.style.opacity = 1.0;
     })
 })
